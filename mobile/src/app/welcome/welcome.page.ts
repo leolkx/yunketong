954a4781f5ae37563d2b64b6b45311fed8dd8ccc
yao-zheng-yi <yao1079543838@gmail.com>
2021-05-13 11:58:02 +0800

    add welcome pages

---
+++ mobile/src/app/welcome/welcome.page.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import { LocalStorageService } from '../shared/services/local-storage.service';
+import { IonSlides } from '@ionic/angular';
+import { Router } from '@angular/router';
+export const App = 'App';
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.page.html',
+  styleUrls: ['./welcome.page.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class WelcomePage implements OnInit {
+  showSkip = true;
+  @ViewChild('slides', {static: false}) slides: IonSlides;
+  constructor(private localStorageService:LocalStorageService, private router: Router) { }
+
+  ngOnInit() {
+  }
+  onSlideWillChange(event) {
+    console.log(event);
+    this.slides.isEnd().then((end) => {
+      this.showSkip = !end;
+    });
+  }
+  onSkip(){
+    this.router.navigateByUrl('\login');
+  }
+}
